11b7dea0ddf6f57ad81897c9aacc37be18401c59
ignacio14.castillor@gmail.com <ignacio.castillo-r@mail.escuelaing.edu.co>
2025-08-21 14:09:18 -0500

    feature: Preparacion - Subir estructura de paquetes del Laboratorio 02

---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/Application.java
@@ -0,0 +1,8 @@
+package edu.dosw.lab;
+
+public class Application {
+    public static main(String[] args) {
+
+        System.out.println("Proyecto Maven configurado y corriendo correctamente");
+    }
+}
